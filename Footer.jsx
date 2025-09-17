52d2d9554c405b705f76112293f0541fd4ff7800
Komal Jadhav <158317145+Komal0032@users.noreply.github.com>
2025-09-17 18:39:26 +0530

    Add files via upload

--- Footer.jsx
+++ Footer.jsx
@@ -54,11 +54,11 @@ const Footer = () => {
             <ul>
               <li>
                 <FaMapMarkerAlt className="contact-icon" />
-                <span>123 Drive Street, Nashik City, AC 12345</span>
+                <span>123 Drive Street, Auto City, AC 12345</span>
               </li>
               <li>
                 <FaPhone className="contact-icon" />
-                <span>(1123227890</span>
+                <span>(123) 456-7890</span>
               </li>
               <li>
                 <FaEnvelope className="contact-icon" />
@@ -86,6 +86,5 @@ const Footer = () => {
     </footer>
   );
 };
-
 
 export default Footer;
